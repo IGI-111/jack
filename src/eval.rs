03353298fdcde784dfea2bc24ada844d19b3b8d6
IGI-111 <igi-111@protonmail.com>
2019-07-26 18:06:49 +0200

    Add package eval
    TODO: decide on policy on fixed point

--- src/eval.rs
+++ src/eval.rs
@@ -23,55 +23,73 @@ impl Value {
     }
 }
 
-struct Context {
-    modules: HashMap<String, Module>,
+pub struct Context<'a> {
+    modules: &'a HashMap<String, Module>,
+}
+
+impl<'a> Context<'a> {
+    pub fn new(modules: &'a HashMap<String, Module>) -> Self {
+        Self { modules }
+    }
+    pub fn get_module(&self, name: &str) -> &Module {
+        self.modules
+            .get(name)
+            .expect(&format!("Unknown module {}", name))
+    }
 }
 
 pub fn eval(expr: &Expression, ctx: &Context) -> Value {
     match expr {
-        Expression::Id(id) => {}
+        Expression::Id(id) => eval(&ctx.get_module(id).export.expr, ctx),
         Expression::BinaryOp(op, a, b) => match op {
-            BinaryOp::Multiply => Value::Int(eval(a).unwrap_int() * eval(b).unwrap_int()),
-            BinaryOp::Divide => Value::Int(eval(a).unwrap_int() / eval(b).unwrap_int()),
-            BinaryOp::Add => Value::Int(eval(a).unwrap_int() + eval(b).unwrap_int()),
-            BinaryOp::Sub => Value::Int(eval(a).unwrap_int() - eval(b).unwrap_int()),
-            BinaryOp::LessThan => Value::Bool(eval(a).unwrap_int() < eval(b).unwrap_int()),
-            BinaryOp::LessThanOrEqual => Value::Bool(eval(a).unwrap_int() <= eval(b).unwrap_int()),
-            BinaryOp::GreaterThan => Value::Bool(eval(a).unwrap_int() > eval(b).unwrap_int()),
-            BinaryOp::GreaterThanOrEqual => {
-                Value::Bool(eval(a).unwrap_int() >= eval(b).unwrap_int())
+            BinaryOp::Multiply => Value::Int(eval(a, ctx).unwrap_int() * eval(b, ctx).unwrap_int()),
+            BinaryOp::Divide => Value::Int(eval(a, ctx).unwrap_int() / eval(b, ctx).unwrap_int()),
+            BinaryOp::Add => Value::Int(eval(a, ctx).unwrap_int() + eval(b, ctx).unwrap_int()),
+            BinaryOp::Sub => Value::Int(eval(a, ctx).unwrap_int() - eval(b, ctx).unwrap_int()),
+            BinaryOp::LessThan => {
+                Value::Bool(eval(a, ctx).unwrap_int() < eval(b, ctx).unwrap_int())
             }
-            BinaryOp::Equal => match (eval(a), eval(b)) {
+            BinaryOp::LessThanOrEqual => {
+                Value::Bool(eval(a, ctx).unwrap_int() <= eval(b, ctx).unwrap_int())
+            }
+            BinaryOp::GreaterThan => {
+                Value::Bool(eval(a, ctx).unwrap_int() > eval(b, ctx).unwrap_int())
+            }
+            BinaryOp::GreaterThanOrEqual => {
+                Value::Bool(eval(a, ctx).unwrap_int() >= eval(b, ctx).unwrap_int())
+            }
+            BinaryOp::Equal => match (eval(a, ctx), eval(b, ctx)) {
                 (Value::Bool(a), Value::Bool(b)) => Value::Bool(a == b),
                 (Value::Int(a), Value::Int(b)) => Value::Bool(a == b),
                 _ => panic!("Invalid type"),
             },
-            BinaryOp::NotEqual => match (eval(a), eval(b)) {
+            BinaryOp::NotEqual => match (eval(a, ctx), eval(b, ctx)) {
                 (Value::Bool(a), Value::Bool(b)) => Value::Bool(a != b),
                 (Value::Int(a), Value::Int(b)) => Value::Bool(a != b),
                 _ => panic!("Invalid type"),
             },
-            BinaryOp::And => Value::Bool(eval(a).unwrap_bool() && eval(b).unwrap_bool()),
-            BinaryOp::Or => Value::Bool(eval(a).unwrap_bool() || eval(b).unwrap_bool()),
+            BinaryOp::And => Value::Bool(eval(a, ctx).unwrap_bool() && eval(b, ctx).unwrap_bool()),
+            BinaryOp::Or => Value::Bool(eval(a, ctx).unwrap_bool() || eval(b, ctx).unwrap_bool()),
         },
         Expression::UnaryOp(op, a) => match op {
-            UnaryOp::Minus => Value::Int(-eval(a).unwrap_int()),
-            UnaryOp::Not => Value::Bool(!eval(a).unwrap_bool()),
+            UnaryOp::Minus => Value::Int(-eval(a, ctx).unwrap_int()),
+            UnaryOp::Not => Value::Bool(!eval(a, ctx).unwrap_bool()),
         },
 
         Expression::Int(val) => Value::Int(*val),
         Expression::Bool(val) => Value::Bool(*val),
         Expression::Conditional(cond, expr, alt) => {
-            if eval(cond).unwrap_bool() {
-                eval(expr)
+            if eval(cond, ctx).unwrap_bool() {
+                eval(expr, ctx)
             } else {
-                eval(alt)
+                eval(alt, ctx)
             }
         }
     }
 }
 
 pub fn run(main_module_name: &str, modules: &HashMap<String, Module>) -> Value {
+    let ctx = Context::new(modules);
     let main_module = modules.get(main_module_name).unwrap();
-    eval(&main_module.export.expr, modules)
+    eval(&main_module.export.expr, &ctx)
 }
